48a8f5940b2b354a8f92996e1f99a343672a4c24
Lodewijk Sioen <lodewijksioen@gmail.com>
2015-04-29 08:45:02 +0200

    Initial Commit

---
+++ src/AspNetBundling.Contrib.Tests/AjaxMinJsMinifyTests.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using Shouldly;
+using System.Web;
+using System.Web.Optimization;
+
+namespace AspNetBundling.Contrib.Tests
+{
+    public class AjaxMinJsMinifyTests
+    {
+        private AjaxMinJsMinify _ajaxMinJsMinify;
+        private BundleContext _context;
+        private BundleCollection _bundleCollection;
+        private Mock<HttpContextBase> _httpContext;
+        private BundleResponse _response;
+        private readonly string _unminifiesJs = "//this is a comment\r\nvar test = \"Hello World\";\r\nconsole.log(test);";
+        private readonly string _minifiedJs = "var test=\"Hello World\";console.log(test)";
+
+        public AjaxMinJsMinifyTests()
+        {
+            _ajaxMinJsMinify = new AjaxMinJsMinify();
+            _httpContext = new Mock<HttpContextBase>();
+            _bundleCollection = new BundleCollection();
+            _context = new BundleContext(_httpContext.Object, _bundleCollection, "~/bundle");
+            _response = new BundleResponse
+            {
+                Content = _unminifiesJs
+            };
+        }
+
+        public void TestMinificationEnabled()
+        {   
+            _ajaxMinJsMinify.Process(_context, _response);
+
+            _response.Content.ShouldBe(_minifiedJs);
+        }
+
+        public void TestMinificationDisabled()
+        {
+            _context.EnableOptimizations = false;
+            _ajaxMinJsMinify.Process(_context, _response);
+
+            _response.Content.ShouldBe(_unminifiesJs);
+        }
+    }
+}
---
+++ src/AspNetBundling.Contrib/AjaxMinJsMinify.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Ajax.Utilities;
+using System.Web.Optimization;
+
+namespace AspNetBundling.Contrib
+{
+    public class AjaxMinJsMinify : IBundleTransform
+    {
+        public void Process(BundleContext context, BundleResponse response)
+        {
+            response.ContentType = "application/javascript";
+
+            if (context.EnableOptimizations)
+            {
+                var minifier = new Minifier();
+                response.Content = minifier.MinifyJavaScript(response.Content);
+            }
+        }
+    }
+}
